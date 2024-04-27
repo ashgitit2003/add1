c9befafe858b63e90f85d71986ac4e0f20884d6c
ashish <ashishsahu2003@gmail.com>
2024-04-27 18:47:22 +0530

    code added again    nowww

---
+++ Add.java
@@ -0,0 +1,9 @@
+public class Add {
+    
+    public static void main(String[] args) {
+        int a = 200;
+        int b = 21;
+        int sum = a + b;
+        System.out.println(a + " + " + b + " = " + sum);
+    }
+}
